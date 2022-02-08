1fb698051a0dfdd6a411a3eacc4efe0f7b9f7e1a
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-02-08 23:12:18 +0100

    sam4sc:scam not working

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1'
   },
+  collectCoverageFrom: ['projects/sam4sc/schematics/**'],
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
 };
